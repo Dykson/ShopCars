2b5f251e53c08ce092e953eb56bb5937382b8223
Dykson <dykson-vd@yandex.ru>
2016-12-16 10:59:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication12
+namespace ProjectShopCars
 {
     class ModelCar // класс Модель автомобиля
     {
