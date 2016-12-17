d74f29a6c3ee68fc35db44180d11c48a37688172
Dykson <dykson-vd@yandex.ru>
2016-12-17 09:37:08 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,91 +8,89 @@ namespace ProjectShopCars
 {
     class ModelCar // класс Модель автомобиля
     {
-        private string nameModel;
+        private string nameModelCar;
 
-        public string NameModel
+        public string NameModelCar
         {
-            set { this.nameModel = value; }
-            get { return nameModel; }
+            set { this.nameModelCar = value; }
+            get { return nameModelCar; }
         }
     }
     class Vehicle  // класс Средство передвижения
     {
         private string nameVehicle;
         private float volumeEngine; // Объём двигателя
-        private int cost;  // Цена
+        private int costVehicle;  // Цена
 
-        public Vehicle(ModelCar modelCar, float volumeEngine, int cost)  // Пользовательский конструктор
+        public Vehicle(ModelCar modelCar, float volumeEngine, int costVehicle)  // Пользовательский конструктор
         {
-            this.nameVehicle = modelCar.NameModel;
+            this.nameVehicle = modelCar.NameModelCar;
             this.volumeEngine = volumeEngine;
-            this.cost = cost;
+            this.costVehicle = costVehicle;
         }
 
-        public string getNameVehicle() //Прочитать Имя средства передвижения
+        public string GetNameVehicle() //Прочитать Имя средства передвижения
         {
             return this.nameVehicle;
         }
 
-        public float getVolumeEngine() //Прочитать Объём двигателя
+        public float GetVolumeEngine() //Прочитать Объём двигателя
         {
             return this.volumeEngine;
         }
 
-        public int getCost() // Прочитать цену
+        public int GetCostVehicle() // Прочитать цену
         {
-            return this.cost;
+            return this.costVehicle;
         }
     }
 
     class ShopCars //Магазин автомобилей
     {
-        private string nameShop;
-        private string addressShop;
+        private string nameShopCars;
+        private string addressShopCars;
 
-        public ShopCars(string nameShop, string addressShop)
+        public ShopCars(string nameShopCars, string addressShopCars)
         {
-            this.nameShop = nameShop;
-            this.addressShop = addressShop;
+            this.nameShopCars = nameShopCars;
+            this.addressShopCars = addressShopCars;
         }
 
-        private Vehicle[] garage = new Vehicle[3]; // cars --> garage   Создаём гараж из трёх мест под Средства передвижения
+        private Vehicle[] garage = new Vehicle[3]; // Cars --> garage   Создаём гараж из трёх мест под Средства передвижения
         private int countPlaces = 0; // Счётчик мест в гараже. Указатель на пустое место в гараже
 
         public void AddCar(Vehicle car)  // Добавить машину в гараж
         {
-            //countPlaces = 0;
             if (countPlaces < garage.Length)
             {
                 this.garage[this.countPlaces] = car; // В гараж помещаем машину
                 this.countPlaces++;
-                Console.WriteLine("Добавлена машина {0}", car.getNameVehicle());
+                Console.WriteLine("Добавлена машина {0}", car.GetNameVehicle());
             }
-            else { Console.WriteLine("Гараж полон. {0} не помещается в гараж", car.getNameVehicle()); }
+            else { Console.WriteLine("Гараж полон. {0} не помещается в гараж", car.GetNameVehicle()); }
         }
-        
         public void ShowAllCars() // Показать все машины
         {
             if (this.countPlaces > 0)
             {
-                Console.WriteLine("В магазине {0} по адресу {1} в наличии следующие машины:", this.getNameShop(), this.getAddressShop());
+                Console.WriteLine("В магазине {0} по адресу {1} в наличии следующие машины:", this.GetNameShop(), this.GetAddressShop());
                 
                 for (int i = 0; i < this.countPlaces; i++)
                 {
-                    Console.WriteLine(this.garage[i].getNameVehicle());
+                    Console.WriteLine(this.garage[i].GetNameVehicle());
                 }
             }
             else { Console.WriteLine("Гараж пуст!"); }
         }
 
-        public string getNameShop() // Прочитать Название Магазина
+        public string GetNameShop() // Прочитать Название Магазина
         {
-            return this.nameShop;
+            return this.nameShopCars;
         }
 
-        public string getAddressShop() // Прочитать Адрес магазина
+        public string GetAddressShop() // Прочитать Адрес магазина
         {
-            return this.addressShop;
+            return this.addressShopCars;
         }
     }
 
@@ -102,24 +100,24 @@ namespace ProjectShopCars
         {
             ModelCar modelCar1 = new ModelCar();
 
-            modelCar1.NameModel = "Фольксваген";
+            modelCar1.NameModelCar = "Фольксваген";
             Vehicle car1 = new Vehicle(modelCar1, 1.4f, 700000);
 
-            modelCar1.NameModel = "Ауди";
+            modelCar1.NameModelCar = "Ауди";
             Vehicle car2 = new Vehicle(modelCar1, 1.6f, 900000);
 
-            modelCar1.NameModel = "Форд";
+            modelCar1.NameModelCar = "Форд";
             Vehicle car3 = new Vehicle(modelCar1, 2.3f, 1100000);
 
-            modelCar1.NameModel = "Мерседес";
+            modelCar1.NameModelCar = "Мерседес";
             Vehicle car4 = new Vehicle(modelCar1, 2.8f, 1600000);
 
 
             ShopCars ShopCars1 = new ShopCars("Мир Авто", "ул. Советская, д.32");
             ShopCars1.AddCar(car1);
-            ShopCars1.AddCar(car2);
-            ShopCars1.AddCar(car3);
-            ShopCars1.AddCar(car4);
+            //ShopCars1.AddCar(car2);
+            //ShopCars1.AddCar(car3);
+            //ShopCars1.AddCar(car4);
 
             Console.WriteLine(new string('-', 30));
             ShopCars1.ShowAllCars();
